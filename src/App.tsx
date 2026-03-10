a9119c45e7d1096b7487a4d9380921e5187b66a7
Wafaa250 <wafaaabosamra78@email.com>
2026-03-10 12:19:13 +0200

    add export to CSV

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import type { ColDef, ICellRendererParams } from "ag-grid-community";
 import { ModuleRegistry, AllCommunityModule } from "ag-grid-community";
-
+import { useState, useRef } from "react";
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 interface RowData {
@@ -16,7 +15,7 @@ interface RowData {
 
 function App() {
   const [currentUserRole, setCurrentUserRole] = useState<"admin" | "user">("user");
-
+  const gridRef = useRef<AgGridReact>(null);
   const [rowData, setRowData] = useState<RowData[]>([
     { name: "wafaa", age: 22, country: "Palestine", role: "admin" },
     { name: "omar",  age: 25, country: "Palestine", role: "user"  },
@@ -43,7 +42,11 @@ const duplicateRow = (rowToCopy: RowData) => {
 const deleteRow = (rowToDelete: RowData) => {
   setRowData((prev) => prev.filter((row) => row !== rowToDelete));
 };
-
+// exportCSV
+const exportCSV = () => {
+  gridRef.current?.api.exportDataAsCsv();
+};
+//sortable: false
   const columnDefs: ColDef<RowData>[] = [
     { field: "name",    editable: () => currentUserRole === "admin", sortable: true  },
     { field: "age",     editable: () => currentUserRole === "admin", sortable: true  },
@@ -70,7 +73,7 @@ const deleteRow = (rowToDelete: RowData) => {
         fontSize: 12,
       }}
     >
-      ⧉ Copy
+      Copy
     </button>
 
     <button
@@ -90,46 +93,62 @@ const deleteRow = (rowToDelete: RowData) => {
     </div>
       ),
     },
-  ];
-
-  return (
-    <div style={{ height: "100vh", padding: "20px" }}>
-      <div style={{ marginBottom: 16, display: "flex", gap: 10 }}>
-        <button
-          onClick={() => setCurrentUserRole("admin")}
-          style={{
-            padding: "8px 20px",
-            background: currentUserRole === "admin" ? "#a3347e" : "#e4d4de",
-            color: currentUserRole === "admin" ? "white" : "black",
-            border: "none", borderRadius: 6, cursor: "pointer",
-          }}
-        >
-          Admin
-        </button>
-        <button
-          onClick={() => setCurrentUserRole("user")}
-          style={{
-            padding: "8px 20px",
-            background: currentUserRole === "user" ? "#a3347e" : "#e4d4de",
-            color: currentUserRole === "user" ? "white" : "black",
-            border: "none", borderRadius: 6, cursor: "pointer",
-          }}
-        >
-          User
-        </button>
-        <span style={{ alignSelf: "center", color: "#b3559e" }}>
-          {currentUserRole === "admin" ? "Editing enabled" : "Read-only"}
-        </span>
-      </div>
-
-      <div className="ag-theme-quartz" style={{ height: 400, width: 935 }}>
-        <AgGridReact
-          rowData={rowData}
-          columnDefs={columnDefs}
-        />
-      </div>
+  ];return (
+  <div style={{ height: "100vh", padding: "20px" }}>
+    <div style={{ marginBottom: 16, display: "flex", gap: 10 }}>
+      <button
+        onClick={() => setCurrentUserRole("admin")}
+        style={{
+          padding: "8px 20px",
+          background: currentUserRole === "admin" ? "#a3347e" : "#e4d4de",
+          color: currentUserRole === "admin" ? "white" : "black",
+          border: "none", borderRadius: 6, cursor: "pointer",
+        }}
+      >
+        Admin
+      </button>
+      <button
+        onClick={() => setCurrentUserRole("user")}
+        style={{
+          padding: "8px 20px",
+          background: currentUserRole === "user" ? "#a3347e" : "#e4d4de",
+          color: currentUserRole === "user" ? "white" : "black",
+          border: "none", borderRadius: 6, cursor: "pointer",
+        }}
+      >
+        User
+      </button>
+      <span style={{ alignSelf: "center", color: "#b3559e" }}>
+        {currentUserRole === "admin" ? "Editing enabled" : "Read-only"}
+      </span>
     </div>
-  );
+
+    <div className="ag-theme-quartz" style={{ height: 400, width: 935 }}>
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+      />
+    </div>
+
+    <div style={{ marginTop: 12 }}>
+      <button
+        onClick={exportCSV}
+        style={{
+          padding: "5px 15px",
+          background: "#7e185f",
+          color: "white",
+          border: "none",
+          borderRadius: 6,
+          cursor: "pointer",
+        }}
+      >
+        Export CSV
+      </button>
+    </div>
+
+  </div>
+);
 }
 
 export default App;
